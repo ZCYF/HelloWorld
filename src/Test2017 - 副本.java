329c4ae8a919d5525019090e87fd15b9aba46544
ZCYF <243531033@qq.com>
2017-11-21 09:33:52 +0800

    second project

---
+++ src/Test2017 - 副本.java
@@ -0,0 +1,37 @@
+package com;
+/**
+ * 
+ * @类名称 Test2017.java
+ * @类描述 <pre></pre>
+ * @作者 zhuangzhichao zhuangzhichao@tansun.com.cn
+ * @创建时间 2017年8月22日 下午2:16:38
+ * @版本 1.00
+ *
+ * @修改记录
+ * <pre>
+ *     版本                       修改人 		修改日期 		 修改内容描述
+ *     ----------------------------------------------
+ *     1.00 	zhuangzhichao 	2017年8月22日             
+ *     ----------------------------------------------
+ * </pre>
+ */
+public class Test2017 {
+
+	public static void test(int i) {
+		if (i == 0) {
+			System.out.println("123");
+		}
+	}
+
+	public static void main(String[] args) {
+		int i = 0;
+		test(i);
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
+		
+	}
+}
